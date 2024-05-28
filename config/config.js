e6ba140eeb68ae5482ac65afda7ed1911a9308e4
AdekunleM <adekunlemichael1319@gmail.com>
2024-05-28 14:36:39 +0100

    second commit

--- config/config.js
+++ config/config.js
@@ -8,6 +8,18 @@ mongoose.connect(db).then(()=> {
     console.log("Database Connected successfully")
 }).catch((error)=> {
     console.log(`Unable to connect to datbase ${error}`);
+    console.error('Error connecting to MongoDB:', error.message);
+ 
+        // Handle specific error conditions
+        if (error.name === 'MongoNetworkError') {
+            console.error('Network error occurred. Check your MongoDB server.');
+        } else if (error.name === 'MongooseServerSelectionError') {
+            console.error('Server selection error. Ensure'
+                + ' MongoDB is running and accessible.');
+        } else {
+            // Handle other types of errors
+            console.error('An unexpected error occurred:', error);
+        }
 })
 
 
